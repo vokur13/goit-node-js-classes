4650a2f4508f556716ddc101339f511a9f1b9131
Volodymyr Kurov <vokur13@gmail.com>
2023-02-20 15:05:14 +0200

    Update userModel.js

--- blogs/src/model/userModel.js
+++ blogs/src/model/userModel.js
@@ -25,9 +25,11 @@ const UserSchema = new Schema(
 
 UserSchema.pre('save', async function (next) {
   const user = this;
-  const hash = await bcrypt.hash(this.password, saltRounds);
-  this.password = hash;
-  next();
+  if (this.isNew || this.isModified) {
+    const hash = await bcrypt.hash(this.password, saltRounds);
+    this.password = hash;
+    next();
+  }
 });
 
 UserSchema.methods.isValidPassword = async function (password) {
